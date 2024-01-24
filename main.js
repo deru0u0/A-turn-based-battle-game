f0fdcaaf73323417755abb8f219769e0fc7709cd
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-24 18:13:10 +0900

    残り体力によって色分けができるようにしました

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 {
     const msgArray = [
+        'ボタンを押して戦闘を開始してください。',
         '〇〇の攻撃。',
         '〇〇からの攻撃！',
         'ターン目',
+        '勝ち!',
+        'ゲームオーバー',
     ]
     let fitness = 100;
     let enemyFitness = 150;
@@ -12,23 +15,28 @@
     const fitnessResult = document.getElementById('fitness');
     const enemyFitnessResult = document.getElementById('enemy-fitness');
     const message = document.querySelector('span');
+    message.textContent = msgArray[0];
     update();
 
     function update() {
         fitnessResult.textContent = `体力:${fitness}`;
         enemyFitnessResult.textContent = `敵の体力:${enemyFitness}`;
-        countMessage.textContent = `${count}${msgArray[2]}`;
+        countMessage.textContent = `${count}${msgArray[3]}`;
         if ( enemyFitness <= 0 ) {
-            console.log('勝ち！');
+            console.log(`${msgArray[4]}`);
         }
-        if ( fitness <= 0 ) {
-            console.log('ゲームオーバー');
+        if ( fitness <= 50) {
+            fitnessResult.classList.add('fiftyofless');
+        } else if ( fitness <= 20 ) {
+            fitnessResult.classList.add('twentyofless');
+        } else if ( fitness <= 0 ) {
+            console.log(`${msgArray[5]}`);
         }
     }
     function enemyAttack() {
         const r_enemyAttack = Math.floor(Math.random()*30);
         fitness = fitness - r_enemyAttack;
-        message.textContent = `${msgArray[1]}${r_enemyAttack}のダメージを受けた`;
+        message.textContent = `${msgArray[2]}${r_enemyAttack}のダメージを受けた`;
         update();
     }
     const attackButton = document.getElementById('attack-button');
@@ -38,7 +46,7 @@
         const r_attack = Math.floor(Math.random()*40);
         enemyFitness = enemyFitness - r_attack;
         update();
-        message.textContent = `${msgArray[0]}${r_attack}のダメージ`;
+        message.textContent = `${msgArray[1]}${r_attack}のダメージ`;
         setTimeout(()=>{
             enemyAttack();
 
